6fe7c01f04164b3a23d99a435d033d9b8b9614c1
adinkramedia <adinkraaudio@outlook.com>
2025-04-19 15:21:01 -0800

    commit

---
+++ stackbit.config.ts
@@ -0,0 +1,34 @@
+// stackbit.config.ts
+import { defineStackbitConfig } from '@stackbit/types';
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
+
+export default defineStackbitConfig({
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+      environment: process.env.CONTENTFUL_ENVIRONMENT!,
+      models: [
+        {
+          name: 'Page',
+          type: 'page',
+          urlPath: '/{slug}',
+        },
+        {
+          name: 'Blog',
+          type: 'page',
+          urlPath: '/news/{slug}',
+        },
+      ],
+    }),
+  ],
+  siteMap: ({ documents }) => {
+    return documents.map((doc) => ({
+      stableId: doc.id,
+      urlPath: `/${doc.slug}`,
+      document: doc,
+      isHomePage: doc.slug === 'index', // Adjust for your homepage slug
+    }));
+  },
+});
